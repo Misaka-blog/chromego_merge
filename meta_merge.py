b8449abd7a3c9351cc1e6b936ff2a9eb655898cd
gavinfly2021 <94218215+gavinfly2021@users.noreply.github.com>
2024-04-18 09:16:00 +0800

    Update meta_merge.py

--- meta_merge.py
+++ meta_merge.py
@@ -264,7 +264,7 @@ def process_xray(data, index):
 
 def update_proxy_groups(config_data, merged_proxies):
     for group in config_data["proxy-groups"]:
-        if group["name"] in ["自动选择", "节点选择"]:
+        if group["name"] in ["♻️ 自动选择", "🚀 节点选择"]:
             if "proxies" not in group or not group["proxies"]:
                 group["proxies"] = [proxy["name"] for proxy in merged_proxies]
             else:
@@ -273,7 +273,7 @@ def update_proxy_groups(config_data, merged_proxies):
 
 def update_warp_proxy_groups(config_warp_data, merged_proxies):
     for group in config_warp_data["proxy-groups"]:
-        if group["name"] in ["自动选择", "手动选择", "负载均衡"]:
+        if group["name"] in ["♻️ 自动选择", "🚀 节点选择", "🔮 负载均衡"]:
             if "proxies" not in group or not group["proxies"]:
                 group["proxies"] = [proxy["name"] for proxy in merged_proxies]
             else:
